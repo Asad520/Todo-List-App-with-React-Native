8cb7bc8b96dac71fc2e2955414d36dba384e41e0
Asad520 <asadbutt667@gmail.com>
2020-09-19 03:26:37 +0500

    Bin icon addd next to todo item

--- components/Todo.js
+++ components/Todo.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { MaterialIcons } from "@expo/vector-icons";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function Todo({ item, pressHandler }) {
   return (
     <TouchableOpacity onPress={() => pressHandler(item.key)}>
-      <Text style={styles.item}>{item.text}</Text>
+      <View style={styles.item}>
+        <MaterialIcons name="delete" color="white" size={20} />
+        <Text style={styles.text}>{item.text}</Text>
+      </View>
     </TouchableOpacity>
   );
 }
@@ -17,5 +21,9 @@ const styles = StyleSheet.create({
     borderWidth: 1.5,
     borderRadius: 10,
     backgroundColor: "#2dadff",
+    flexDirection: "row",
+  },
+  text: {
+    marginLeft: 10,
   },
 });
